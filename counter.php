c28268b7bf0fbcb9cda71cc1d1178ff52c6c8c9f
Carlos Ballesteros Velasco <soywiz@gmail.com>
2016-08-04 00:28:05 +0200

    First commit

---
+++ counter.php
@@ -0,0 +1,14 @@
+<? 
+$filename = "counter.txt"; 
+$fp = fopen( $filename,"r"); 
+$Old = fread($fp, 100); 
+fclose( $fp ); 
+$Old = split ("=", $Old, 5);
+$NewCount = $Old[1] + '1';
+$New = "counter=$NewCount";
+$fp = fopen( $filename,"w+"); 
+if (flock($fp, 2)) { 
+fwrite($fp, $New, 100); } 
+fclose( $fp ); 
+print "counter=$NewCount"; 
+?> 
